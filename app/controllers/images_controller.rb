a422367629cf7e2f1c272a9192e89f7569fbfec5
emottk <emily.kowal@postlight.com>
2016-10-09 14:06:48 -0400

    update Rails 5, add image controller and template

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,12 +1,25 @@
 class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @image = Image.new(params[:image])
+    @image = Image.new(article_params)
 
     @image.save
     redirect_to @image
+  end
+
+private
+  def article_params
+    params.require(:image).permit(:title, :url, :description)
   end
 
 end
