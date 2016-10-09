2e6dcc36db92154a23ce110b093b2fd070fb1daa
emottk <emily.kowal@postlight.com>
2016-10-09 12:13:29 -0400

    add controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  
+
+  resources :images
+
+  get 'contact/index'
+
+  get 'gallery/index'
+
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
