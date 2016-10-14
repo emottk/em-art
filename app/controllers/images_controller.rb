ce593979ef861ebb17a6d55e7a6f3d8893ac9f9f
emottk <emily.kowal@postlight.com>
2016-10-14 09:40:46 -0400

    edit views and gallery

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,6 @@
 class ImagesController < ApplicationController
+
+
   def index
     @images = Image.all
   end
@@ -8,13 +10,38 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.new
+  end
+
+  def edit
+    @image = Image.find(params[:id])
   end
 
   def create
     @image = Image.new(article_params)
 
-    @image.save
-    redirect_to @image
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @image = Image.find(params[:id])
+
+    if @image.update(image_params)
+      redirect_to @image
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+
+    redirect_to images_path
   end
 
 private
